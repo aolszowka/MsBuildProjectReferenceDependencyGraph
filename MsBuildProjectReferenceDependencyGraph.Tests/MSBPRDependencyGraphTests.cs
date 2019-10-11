a46f5eb99a3391f0052110bacbb84f280f12b00d
Ace Olszowka <aolszowka@gmail.com>
2019-10-11 11:28:14 -0600

    Added some simple unit tests

---
+++ MsBuildProjectReferenceDependencyGraph.Tests/MSBPRDependencyGraphTests.cs
@@ -0,0 +1,116 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MSBPRDependencyGraphTests.cs" company="Ace Olszowka">
+// Copyright (c) 2019 Ace Olszowka.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MsBuildProjectReferenceDependencyGraph.Tests
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.IO;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class MSBPRDependencyGraphTests
+    {
+        [TestCaseSource(typeof(ResolveAssemblyReferenceTests))]
+        public void ResolveAssemblyReferenceDependencies(IEnumerable<string> targetProjects, Dictionary<string,IEnumerable<string>> expected)
+        {
+            Dictionary<string, IEnumerable<string>> actual = MSBPRDependencyGraph.ResolveAssemblyReferenceDependencies(targetProjects);
+
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+    }
+
+    internal class ResolveAssemblyReferenceTests : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new
+                TestCaseData
+                (
+                    new string[] { Path.Combine(TestContext.CurrentContext.TestDirectory,"TestProjects", "ProjectA.csproj") },
+                    new Dictionary<string, IEnumerable<string>>
+                    {
+                        {
+                            Path.Combine(TestContext.CurrentContext.TestDirectory,"TestProjects", "ProjectA.csproj"),
+                            new string[]
+                            {
+                                "log4net",
+                                "Moq",
+                                "System",
+                                "System.Core",
+                                "System.Data",
+                                "System.Data.DataSetExtensions",
+                                "System.Data.Linq",
+                                "System.Drawing",
+                                "System.Transactions",
+                                "System.Web.Services",
+                                "System.Windows.Forms",
+                                "System.Xml",
+                                "System.Xml.Linq",
+                                "xfnlnet"
+                            }
+                        }
+                    }
+                ).SetName("{m}_SingleTestInput");
+            yield return new
+                TestCaseData
+                (
+                    new string[]
+                    {
+                        Path.Combine(TestContext.CurrentContext.TestDirectory,"TestProjects", "ProjectA.csproj"),
+                        Path.Combine(TestContext.CurrentContext.TestDirectory,"TestProjects", "ProjectB.csproj")
+                    },
+                    new Dictionary<string, IEnumerable<string>>
+                    {
+                        {
+                            Path.Combine(TestContext.CurrentContext.TestDirectory,"TestProjects", "ProjectA.csproj"),
+                            new string[]
+                            {
+                                "log4net",
+                                "Moq",
+                                "System",
+                                "System.Core",
+                                "System.Data",
+                                "System.Data.DataSetExtensions",
+                                "System.Data.Linq",
+                                "System.Drawing",
+                                "System.Transactions",
+                                "System.Web.Services",
+                                "System.Windows.Forms",
+                                "System.Xml",
+                                "System.Xml.Linq",
+                                "xfnlnet"
+                            }
+                        },
+                        {
+                            Path.Combine(TestContext.CurrentContext.TestDirectory,"TestProjects", "ProjectB.csproj"),
+                            new string[]
+                            {
+                                "Microsoft.VisualBasic",
+                                "System",
+                                "System.Core",
+                                "System.Data",
+                                "System.Data.DataSetExtensions",
+                                "System.Data.Linq",
+                                "System.Drawing",
+                                "System.EnterpriseServices",
+                                "System.Runtime.Serialization",
+                                "System.Transactions",
+                                "System.Web",
+                                "System.Web.Services",
+                                "System.Windows.Forms",
+                                "System.Xml",
+                                "System.Xml.Linq",
+                                "xfnlnet"
+                            }
+                        }
+                    }
+                ).SetName("{m}_MultipleInputs");
+
+        }
+    }
+}
---
+++ MsBuildProjectReferenceDependencyGraph.Tests/MSBuildUtilitiesTests.cs
@@ -0,0 +1,53 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MSBuildUtilitiesTests.cs" company="Ace Olszowka">
+// Copyright (c) 2019 Ace Olszowka.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MsBuildProjectReferenceDependencyGraph.Tests
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    class MSBuildUtilitiesTests
+    {
+        [TestCaseSource(typeof(AssemblyReferencesTestCases))]
+        public void AssemblyReferences(string targetProject, IEnumerable<string> expected)
+        {
+            IEnumerable<string> actual = MSBuildUtilities.AssemblyReferences(targetProject);
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+    }
+
+    internal class AssemblyReferencesTestCases : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new
+                TestCaseData
+                (
+                    @"S:\TimsSVN\8x\Trunk\Dotnet\Source\Framework\BusinessObjects\Tests\ComputersUnlimited.Tests.BusinessObjects.csproj",
+                    new string[]
+                    {
+                        "log4net",
+                        "Moq",
+                        "System",
+                        "System.Core",
+                        "System.Data",
+                        "System.Data.DataSetExtensions",
+                        "System.Data.Linq",
+                        "System.Drawing",
+                        "System.Transactions",
+                        "System.Web.Services",
+                        "System.Windows.Forms",
+                        "System.Xml",
+                        "System.Xml.Linq",
+                        "xfnlnet"
+                    }
+                ).SetName("{m}_TestCase1");
+        }
+    }
+}
