f0b8ffdd8ff38f60aad9e500d8bf9eab42d51f72
Ace Olszowka <aolszowka@gmail.com>
2020-08-20 14:14:32 -0600

    Issue 9 refactor options passing (#12)
    Extracted options passing to its own DTO and added unit tests to validate that this works

---
+++ MsBuildProjectReferenceDependencyGraph/MSBPROptions.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MSBPROptions.cs" company="Ace Olszowka">
+// Copyright (c) 2020 Ace Olszowka.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MsBuildProjectReferenceDependencyGraph
+{
+    /// <summary>
+    /// Class to store options that control how MsBuildProjectReferenceDependencyGraph operates.
+    /// </summary>
+    public class MSBPROptions
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether to anonymize names within the graph.
+        /// </summary>
+        public bool AnonymizeNames { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to sort the graph.
+        /// </summary>
+        public bool SortProjects { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to show Assembly References
+        /// </summary>
+        public bool ShowAssemblyReferences { get; set; }
+    }
+}
