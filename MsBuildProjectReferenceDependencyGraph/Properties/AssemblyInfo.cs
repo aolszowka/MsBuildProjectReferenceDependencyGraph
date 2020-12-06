774fb8dc559e7174a8a82bb0a2b2a1597cbe125d
Ace Olszowka <aolszowka@gmail.com>
2020-12-05 17:14:19 -0700

    Bumpin' Versions

--- MsBuildProjectReferenceDependencyGraph/Properties/AssemblyInfo.cs
+++ MsBuildProjectReferenceDependencyGraph/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MsBuildProjectReferenceDependencyGraph")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Generate a DOT Graph for a Project or Solutuion.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ace Olszowka (GitHub @aolszowka)")]
 [assembly: AssemblyProduct("MsBuildProjectReferenceDependencyGraph")]
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.16")]
-[assembly: AssemblyFileVersion("1.0.0.16")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
